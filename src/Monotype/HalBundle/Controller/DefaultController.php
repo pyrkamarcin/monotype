0191da0eecd7f8a571b20a3515e49ad9abe95343
pyrka.marcin@gmail.com <Oberon13@>
2016-02-24 22:22:08 +0100

    Initial commit

---
+++ src/Monotype/HalBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Monotype\HalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MonotypeHalBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Monotype/HalBundle/MonotypeHalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Monotype\HalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MonotypeHalBundle extends Bundle
+{
+}
