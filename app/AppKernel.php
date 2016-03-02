dace4fc20830a56af4d0df0fe6ce6a23f2ed0151
jetalone <pyrka.marcin@gmail.com>
2016-03-02 08:28:08 +0100

    Update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Monotype\Bundle\HalBundle\MonotypeHalBundle(),
             new MEF\SocketBundle\MEFSocketBundle(),
-            new Monotype\Bundle\DeamonBundle\DeamonBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
