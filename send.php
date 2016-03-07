4153dc6082f2dd5e63f9af47d75276c7985192e4
jetalone <pyrka.marcin@gmail.com>
2016-03-07 07:34:07 +0100

    Update

--- send.php
+++ send.php
@@ -9,9 +9,6 @@ set_time_limit(0);
  * as it comes in. */
 ob_implicit_flush();
 
-$address = '192.168.100.102';
-$port = 4001;
-
 require_once "app/autoload.php";
 
 //$socketStream = new \MEF\SocketBundle\Socket\SocketStream(
@@ -28,7 +25,7 @@ require_once "app/autoload.php";
 
 //$loop = React\EventLoop\Factory::create();
 //
-//$client = stream_socket_client('tcp://192.168.100.102:4001');
+//$client = stream_socket_client('tcp://192.168.100.101:4001');
 //$conn = new React\Stream\Stream($client, $loop);
 //$conn->pipe(new React\Stream\Stream(STDOUT, $loop));
 //
@@ -38,13 +35,12 @@ require_once "app/autoload.php";
 //
 //$loop->run();
 
-$socket = new \Monotype\Bundle\HalBundle\Utils\Socket('tcp', '192.168.100.102', '4001');
+$socket = new \Monotype\Bundle\HalBundle\Utils\Socket('tcp', '192.168.100.101', '4001');
 $socket->openStream();
 
 $file = file_get_contents('program');
 
 $socket->write($file, filesize('program'));
-$socket->write("");
 
 echo "ok!\r\n";
 $socket->closeStream();
