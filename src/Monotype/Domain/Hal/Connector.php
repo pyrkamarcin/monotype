eb0d5f93d003d0a679b16b0f1af5083dda5f634d
jetalone <pyrka.marcin@gmail.com>
2016-03-07 10:53:25 +0100

    Update

--- src/Monotype/Domain/Hal/Connector.php
+++ src/Monotype/Domain/Hal/Connector.php
@@ -4,11 +4,25 @@ namespace Monotype\Domain\Hal;
 
 use Monotype\Domain\Hal\Machine;
 
+/**
+ * Class Connector
+ * @package Monotype\Domain\Hal
+ */
 class Connector
 {
+    /**
+     * @var \Monotype\Domain\Hal\Machine
+     */
     protected $machine;
+    /**
+     * @var
+     */
     protected $socket;
 
+    /**
+     * Connector constructor.
+     * @param \Monotype\Domain\Hal\Machine $machine
+     */
     public function __construct(Machine $machine)
     {
         $this->machine = $machine;
--- src/Monotype/Domain/Hal/Machine.php
+++ src/Monotype/Domain/Hal/Machine.php
@@ -2,10 +2,21 @@
 
 namespace Monotype\Domain\Hal;
 
+/**
+ * Class Machine
+ * @package Monotype\Domain\Hal
+ */
 class Machine
 {
+    /**
+     * @var
+     */
     public $parametrs;
 
+    /**
+     * Machine constructor.
+     * @param $id
+     */
     public function __construct($id)
     {
     }
