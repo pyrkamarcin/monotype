b8e61997336a7e3b8a1fed879a66a7d8f048e4f8
jetalone <pyrka.marcin@gmail.com>
2016-03-07 11:03:53 +0100

    Update

---
+++ src/Monotype/Bundle/BowmanBundle/Entity/Process.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Monotype\Bundle\BowmanBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Process
+ *
+ * @ORM\Table(name="Process")
+ * @ORM\Entity(repositoryClass="Monotype\Bundle\BowmanBundle\Repository\ProcessRepository")
+ */
+class Process
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pid", type="string", length=255)
+     */
+    private $pid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="script", type="string", length=1024)
+     */
+    private $script;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uid", type="string", length=255)
+     */
+    private $uid;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get pid
+     *
+     * @return string
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    /**
+     * Set pid
+     *
+     * @param string $pid
+     *
+     * @return Process
+     */
+    public function setPid($pid)
+    {
+        $this->pid = $pid;
+
+        return $this;
+    }
+
+    /**
+     * Get script
+     *
+     * @return string
+     */
+    public function getScript()
+    {
+        return $this->script;
+    }
+
+    /**
+     * Set script
+     *
+     * @param string $script
+     *
+     * @return Process
+     */
+    public function setScript($script)
+    {
+        $this->script = $script;
+
+        return $this;
+    }
+
+    /**
+     * Get uid
+     *
+     * @return string
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * Set uid
+     *
+     * @param string $uid
+     *
+     * @return Process
+     */
+    public function setUid($uid)
+    {
+        $this->uid = $uid;
+
+        return $this;
+    }
+}
+
