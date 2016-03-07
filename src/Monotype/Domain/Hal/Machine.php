4e5eb13be57e8a9a20523c543164d78a3a005f14
jetalone <pyrka.marcin@gmail.com>
2016-03-07 12:36:49 +0100

    Update

--- src/Monotype/Domain/Hal/Machine.php
+++ src/Monotype/Domain/Hal/Machine.php
@@ -11,7 +11,7 @@ class Machine
     /**
      * @var
      */
-    public $parametrs;
+    private $parametrs = array();
 
     /**
      * Machine constructor.
@@ -19,5 +19,38 @@ class Machine
      */
     public function __construct($id)
     {
+        /**
+         * @TODO przenieść ustawienia w zewnętrzne miejsce? może entity?
+         */
+        switch ($id) {
+            case '1':
+                $this->parametrs = array(
+                    'id' => '1',
+                    'name' => 'CTX1',
+                    'protocol' => 'tcp',
+                    'address' => '192.168.100.101',
+                    'port' => '4001',
+                    'location' => 'main'
+                );
+                break;
+            case '2':
+                $this->parametrs = array(
+                    'id' => '2',
+                    'name' => 'CTX2',
+                    'protocol' => 'tcp',
+                    'address' => '192.168.100.102',
+                    'port' => '4001',
+                    'location' => 'main'
+                );
+                break;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParametrs()
+    {
+        return $this->parametrs;
     }
 }
--- tests/Monotype/Domain/Hal/DumperTest.php
+++ tests/Monotype/Domain/Hal/DumperTest.php
@@ -5,9 +5,13 @@ namespace Tests\Monotype\Domain\Hal;
 use Monotype\Domain\Hal\Dumper;
 use Monotype\Domain\Hal\Dumper\Stock;
 
+/**
+ * Class DumperTest
+ * @package Tests\Monotype\Domain\Hal
+ */
 class DumperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testPush()
+    public function testStockize()
     {
         $dumper = new Dumper(new Stock());
 
