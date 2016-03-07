62928cc7398aaa91183003f479e5525e1a32dce5
jetalone <pyrka.marcin@gmail.com>
2016-03-07 10:56:01 +0100

    Update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Monotype\Bundle\HalBundle\MonotypeHalBundle(),
+            new Monotype\Bundle\BowmanBundle\MonotypeBowmanBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
