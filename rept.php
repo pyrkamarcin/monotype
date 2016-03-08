08c9857ded8fe939f237b45bf50f58d0c81954b4
jetalone <pyrka.marcin@gmail.com>
2016-03-08 09:14:09 +0100

    Update

--- rept.php
+++ rept.php
@@ -1,3 +1,14 @@
 <?php
 
-$rept = new \Monotype\Domain\Hal\Repeater();
+$clients = array();
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_bind($socket, '127.0.0.1', 4001);
+socket_listen($socket);
+socket_set_nonblock($socket);
+
+while (true) {
+    if (($newc = socket_accept($socket)) !== false) {
+        echo "Client $newc has connected\n";
+        $clients[] = $newc;
+    }
+}
--- src/Monotype/Domain/Hal/Repeater.php
+++ src/Monotype/Domain/Hal/Repeater.php
@@ -9,6 +9,27 @@ namespace Monotype\Domain\Hal;
 class Repeater
 {
 
+    /**
+     * Coś takiego dobrze działa:
+     *
+     *
+     *
+     * $clients = array();
+     * $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+     * socket_bind($socket, '127.0.0.1', 4001);
+     * socket_listen($socket);
+     * socket_set_nonblock($socket);
+     *
+     * while (true) {
+     * if (($newc = socket_accept($socket)) !== false) {
+     * echo "Client $newc has connected\n";
+     * $clients[] = $newc;
+     * }
+     * }
+     *
+     * No i z tego trzeba zrobić Repeater :)
+     */
+
     public $socket;
 
     public function __construct()
