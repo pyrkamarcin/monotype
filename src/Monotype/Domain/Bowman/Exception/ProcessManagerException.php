4bbf5bc03ae9e8713e8f806bcba51817dea7b437
jetalone <pyrka.marcin@gmail.com>
2016-03-14 07:36:14 +0100

    Update

---
+++ src/Monotype/Domain/Bowman/Exception/ProcessManagerException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Monotype\Domain\Bowman\Exception;
+
+/**
+ * Class ProcessManagerException
+ * @package Monotype\Domain\Bowman\Exception
+ */
+class ProcessManagerException extends \RuntimeException
+{
+    //
+}
