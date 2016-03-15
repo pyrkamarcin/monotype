ee9568c54f41bbf31a2b55a6ba443c1c73673b02
jetalone <pyrka.marcin@gmail.com>
2016-03-15 09:29:14 +0100

    Update

--- src/Monotype/Domain/Hal/Repository.php
+++ src/Monotype/Domain/Hal/Repository.php
@@ -22,11 +22,21 @@ class Repository
         $this->path = $path;
     }
 
+    /**
+     * @param File $file
+     * @return bool
+     */
     public function push(File $file)
     {
+        return true;
     }
 
+    /**
+     * @param File $file
+     * @return bool
+     */
     public function pull(File $file)
     {
+        return true;
     }
 }
