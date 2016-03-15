cf2937beb20f01208c75d3d5b6e9ccf3f45e8c50
jetalone <pyrka.marcin@gmail.com>
2016-03-15 12:25:29 +0100

    Update

--- react.php
+++ react.php
@@ -8,11 +8,10 @@ $socket = new \React\Socket\Server($loop);
 $socket->listen(4001, '0.0.0.0');
 
 $stock = new \Monotype\Domain\Hal\Dumper(new \Monotype\Domain\Hal\Dumper\Stock());
+$buffer = '';
 
-$socket->on('connection', function (\React\Socket\Connection $conn) use ($stock) {
-//    $conn->write('Hello state your resolve' . PHP_EOL);
+$socket->on('connection', function (\React\Socket\Connection $conn) use ($buffer, $stock) {
 
-    $buffer = '';
     $conn->on('data', function ($data) use ($conn, $buffer, $stock) {
 
         $buffer .= $data;
