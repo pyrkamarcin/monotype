3851f50c810c72a56093f2ecbf9e7313ae3f2504
jetalone <pyrka.marcin@gmail.com>
2016-03-15 09:28:03 +0100

    Update

--- src/Monotype/Domain/Hal/Repository.php
+++ src/Monotype/Domain/Hal/Repository.php
@@ -2,7 +2,31 @@
 
 namespace Monotype\Domain\Hal;
 
+/**
+ * Class Repository
+ * @package Monotype\Domain\Hal
+ */
 class Repository
 {
+    /**
+     * @var
+     */
+    public $path;
 
+    /**
+     * Repository constructor.
+     * @param $path
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function push(File $file)
+    {
+    }
+
+    public function pull(File $file)
+    {
+    }
 }
