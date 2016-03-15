f466af77872dba211cc5458eafe0db50d2e0ce5b
JetAlone <pyrka.marcin@gmail.com>
2016-03-15 08:30:20 +0100

    Update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Processing\Bundle\ProcessingBundle\ProcessingBundle(),
             new AppBundle\AppBundle(),
             new Monotype\Bundle\HalBundle\MonotypeHalBundle(),
             new Monotype\Bundle\BowmanBundle\MonotypeBowmanBundle(),
