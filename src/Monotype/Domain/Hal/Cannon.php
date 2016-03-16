27e37b2eeb4fafd530ec6bbb8cc2a8b203b48f5b
jetalone <pyrka.marcin@gmail.com>
2016-03-16 10:41:22 +0100

    Update

--- src/Monotype/Domain/Hal/Cannon.php
+++ src/Monotype/Domain/Hal/Cannon.php
@@ -2,17 +2,34 @@
 
 namespace Monotype\Domain\Hal;
 
+/**
+ * Class Cannon
+ * @package Monotype\Domain\Hal
+ */
 class Cannon
 {
+    /**
+     * @var mixed
+     */
     public $address;
+    /**
+     * @var mixed
+     */
     public $port;
 
+    /**
+     * Cannon constructor.
+     * @param Machine $machine
+     */
     public function __construct(Machine $machine)
     {
         $this->address = $machine->getAddress();
         $this->port = $machine->getPort();
     }
 
+    /**
+     * @param $data
+     */
     public function send($data)
     {
         $fp = stream_socket_client($this->address . ":" . $this->port, $errno, $errstr, $this->port);
