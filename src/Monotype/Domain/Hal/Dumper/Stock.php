8dbc8e84d080f26d6c161ae7b84a30748f87946f
jetalone <pyrka.marcin@gmail.com>
2016-03-22 09:46:07 +0100

    Update Stocks

--- src/Monotype/Domain/Hal/Dumper/Stock.php
+++ src/Monotype/Domain/Hal/Dumper/Stock.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Container;
  * Class Stock
  * @package Monotype\Domain\Hal\Dumper
  */
-class Stock extends ContainerAwareLoader
+class Stock implements StockInterface
 {
 
     /**
