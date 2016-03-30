728150814f82f92208fbc9c1171251a937ce8f6f
jetalone <pyrka.marcin@gmail.com>
2016-03-30 13:02:01 +0200

    Update

--- recive.php
+++ recive.php
@@ -7,7 +7,7 @@ $loop = React\EventLoop\Factory::create();
 
 $client = stream_socket_client('tcp://192.168.100.113:4001');
 $conn = new React\Stream\Stream($client, $loop);
-$conn->pipe(new React\Stream\Stream(STDOUT, $loop));
+//$conn->pipe(new React\Stream\Stream(STDOUT, $loop));
 $conn->on('data', function ($data) use ($conn) {
     echo $data;
 });
