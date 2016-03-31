13910b41676fc1638ba07a25aeb5ccf83274f4f6
jetalone <pyrka.marcin@gmail.com>
2016-03-31 07:38:55 +0200

    Update

--- src/Monotype/Bundle/BowmanBundle/Controller/SynchroController.php
+++ src/Monotype/Bundle/BowmanBundle/Controller/SynchroController.php
@@ -5,15 +5,17 @@ namespace Monotype\Bundle\BowmanBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/synchro")
+ */
 class SynchroController extends Controller
 {
     /**
-     * @Route("/synchro/showdiff")
+     * @Route("/showdiff")
      */
     public function ShowDiffAction()
     {
-        return $this->render('MonotypeBowmanBundle:Synchro:show_diff.html.twig', array(
-            // ...
+        return $this->render('MonotypeBowmanBundle:Synchro:show_diff.html.twig', array(// ...
         ));
     }
 
