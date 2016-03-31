b7cc19c85e779017ae3476e8e3cabb7379091d69
jetalone <pyrka.marcin@gmail.com>
2016-03-31 11:49:40 +0200

    Update

--- src/Monotype/Domain/Hal/Dumper/Stock.php
+++ src/Monotype/Domain/Hal/Dumper/Stock.php
@@ -96,7 +96,7 @@ class Stock implements StockInterface
     {
 //        $this->setHash(Uuid::uuid1() . "_" . Uuid::uuid5(Uuid::NAMESPACE_DNS, $data));
 //        $this->setHash(Uuid::uuid1());
-        file_put_contents($this->getPath() . 'files', $data, FILE_APPEND);
+        file_put_contents($this->getPath(), $data, FILE_APPEND);
 
         return $this->getUniqId();
     }
