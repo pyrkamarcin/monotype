6a72e3566aae2041f2684b9757f38fe86fb094ab
jetalone <pyrka.marcin@gmail.com>
2016-04-04 10:40:17 +0200

    Update

--- src/Monotype/Bundle/HalBundle/Command/ProcessStartCommand.php
+++ src/Monotype/Bundle/HalBundle/Command/ProcessStartCommand.php
@@ -38,6 +38,15 @@ class ProcessStartCommand extends ContainerAwareCommand
 
         $pid = $process->getPid();
 
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $entity_process = new \Monotype\Bundle\BowmanBundle\Entity\Process();
+        $entity_process->setPid($process->getPid());
+        $entity_process->setScript($input->getArgument('argument'));
+        $entity_process->setUid('manual');
+
+        $entityManager->persist($entity_process);
+        $entityManager->flush();
+
         $output->writeln('Command "' . $input->getArgument('argument') . '" result:');
         $output->writeln('PID: ' . $pid . PHP_EOL);
 
