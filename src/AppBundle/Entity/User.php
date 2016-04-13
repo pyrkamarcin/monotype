b9004ccbaf61ae5714d589184eaac61f4b1b675e
jetalone <pyrka.marcin@gmail.com>
2016-04-13 09:06:29 +0200

    Revert "Update"
    This reverts commit b0adb843d5be6900913f9523d96ab66913e307da.

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cardCode", type="string", length=255, unique=true)
+     */
+    protected $cardCode;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
