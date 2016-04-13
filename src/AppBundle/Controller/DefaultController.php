5f84158c3480de0b860cb47413fe60952e7fdb00
jetalone <pyrka.marcin@gmail.com>
2016-04-13 09:07:52 +0200

    Update

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+        ]);
+    }
+}
