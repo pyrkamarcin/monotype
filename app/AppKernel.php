364cc509850f89e038ffd5c333f052b4ed8c678a
jetalone <pyrka.marcin@gmail.com>
2016-05-24 09:01:11 +0200

    Update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
             new Monotype\Bundle\DirectControllBundle\MonotypeDirectControllBundle(),
             new Monotype\Bundle\PosBundle\MonotypePosBundle(),
 
