2e833edaa9d08a97d2d225bdf8c593e9a6af11c3
JetAlone <pyrka.marcin@gmail.com>
2016-06-21 23:19:22 +0200

    Update

---
+++ src/Monotype/Bundle/ManagerBundle/Form/CustomerType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Monotype\Bundle\ManagerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('shortName')
+            ->add('description');
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Monotype\Bundle\ManagerBundle\Entity\Customer'
+        ));
+    }
+}
