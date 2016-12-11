ece2354ae24111c7cce0bbdfa8f43f9ec886b6c9
JetAlone <pyrka.marcin@gmail.com>
2016-12-11 21:45:26 +0100

    Update

--- recive.php
+++ recive.php
@@ -3,5 +3,5 @@
 $loader = require __DIR__ . '/vendor/autoload.php';
 $factory = new \Socket\Raw\Factory();
 
-$socket = $factory->createClient('tcp://192.168.100.113:4001');
-echo($socket->read(16));
+$socket = $factory->createClient('tcp://192.168.0.113:4001');
+var_dump($socket->read(4));
--- send.php
+++ send.php
@@ -3,6 +3,6 @@
 $loader = require __DIR__ . '/vendor/autoload.php';
 $factory = new \Socket\Raw\Factory();
 
-$socket = $factory->createClient('tcp://192.168.100.114:4001');
+$socket = $factory->createClient('tcp://192.168.0.114:4001');
 $socket->write('test');
 $socket->close();
