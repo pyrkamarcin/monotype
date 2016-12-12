d57afd42ead103d2dc7b0167b933adf41d1cbd96
pyrkamarcin <pyrka.marcin@gmail.com>
2016-12-12 08:56:20 +0100

    Update Add TLBundle

---
+++ src/Monotype/Bundle/TransportLayerBundle/Command/ServerRunCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Monotype\Bundle\TransportLayerBundle\Command;
+
+use Monotype\Utils\ServerListening;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ServerRunCommand
+ * @package Monotype\Bundle\TransportLayerBundle\Command
+ */
+class ServerRunCommand extends ContainerAwareCommand
+{
+    /**
+     *
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('server:run')
+            ->setDescription('...')
+            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('UDP Server starting...');
+
+        $server = new ServerListening('0.0.0.0', 4001);
+        $server->run();
+    }
+}
---
+++ src/Monotype/Utils/ServerListening.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Monotype\Utils;
+
+/**
+ * Class ServerListening
+ * @package Monotype\Utils
+ */
+class ServerListening
+{
+    /**
+     * @var \React\EventLoop\ExtEventLoop|\React\EventLoop\LibEventLoop|\React\EventLoop\LibEvLoop|\React\EventLoop\StreamSelectLoop
+     */
+    protected $loop;
+
+    /**
+     * @var \React\Datagram\Factory
+     */
+    protected $factory;
+
+    /**
+     * ServerListening constructor.
+     * @param string $host
+     * @param int $port
+     */
+    public function __construct($host = 'localhost', $port = 4001)
+    {
+        $this->loop = \React\EventLoop\Factory::create();
+
+        $this->factory = new \React\Datagram\Factory($this->loop);
+
+        $this->factory->createServer($host . ':' . $port)->then(function (\React\Datagram\Socket $client) {
+            $client->send('first');
+
+            $client->on('message', function ($message, $serverAddress, $client) {
+                echo 'received "' . $message . '" from ' . $serverAddress . '(' . $client . ')' . PHP_EOL;
+            });
+        });
+    }
+
+    /**
+     *
+     */
+    public function run()
+    {
+        $this->loop->run();
+    }
+}
